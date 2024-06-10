27a53977180bad45992143a28b03acae8c5601de
fkhan313 <farhan.khan615@gmail.com>
2024-06-10 18:52:57 -0400

    added constant class

---
+++ src/main/java/com/qa/automation/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.qa.automation.constants;
+
+public class AppConstants {
+
+    public static final int DEFAULT_SHORT_TIME_OUT =5;
+    public static final int DEFAULT_MEDIUM_TIME_OUT =10;
+    public static final int DEFAULT_LONG_TIME_OUT =20;
+
+}
